3aee9ff8734d8c51d8054346cc459d196e553e78
Luiz G <guga.santos0810@gmail.com>
2023-08-11 15:51:36 -0300

    refactor: update

--- Exercício 1/2/e12.cpp
+++ Exercício 1/2/e12.cpp
@@ -1,10 +1,32 @@
 #include <stdio.h>
 #include <locale.h>
 
+int verifAnoBissexto(int yyyy){
+
+    if(yyyy%4 == 0){
+
+        return 1;
+
+    }
+    else{
+
+        return 0;
+
+    }
+}
+
 int main(){
 
     setlocale(LC_ALL, "Portuguese_Brazil");
 
-    printf("testando!");
+    int dd1, mm1, yyyy, dd2, mm2;
+
+    //printf("Digite a primeira data no formato dd/mm/yyyy: ");
+    //scanf("%i%*c%i%*c%i", &dd1, &mm1, &yyyy);
+
+    //printf("Digite a segunda data no formato dd/mm/yyyy: ");
+    //scanf("%i%*c%i%*c%*i", &dd2, &mm2);
+
+    printf("Tá louco");
 
 }
