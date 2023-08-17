6af9b55ee3b2fee4dd8ff09d042266dc7f039a12
Luiz G <guga.santos0810@gmail.com>
2023-08-16 22:30:13 -0300

    refactor: update

---
+++ capicua/capicua.cpp
@@ -0,0 +1,106 @@
+#include <stdio.h>
+
+int power(int base, int expo){
+	
+	int result = 1;
+	
+	if(expo == 0){
+		
+	}
+	else{
+		
+		for(int y = 0; y < expo; y++){
+		
+			result = result*base;
+		
+		}	
+		
+	}
+
+	return result;
+}
+
+int numAlgarismos(int value){
+	
+	int result = 0;
+	
+	for(int y = 1; value/y >= 1; y = y*10){
+		
+		result++;
+		
+	}
+	
+	return result;
+	
+}
+
+void separAlgarismos(int value, int qnt, int *vet){
+	
+	qnt--;
+	
+	int divisor = power(10, qnt), control = qnt;
+	
+	for(int y = divisor; y > 0; y = y/10){
+		
+		vet[control] = value/y;
+		value = value%y;
+		
+		control--;
+		
+	}
+}
+
+int invertAlgarismos(int qnt, int *vet){
+	
+	qnt--;
+	
+	int multiplier = power(10, qnt), soma = 0;
+	
+	for(int y = 0; y <= qnt; y++){
+		
+		vet[y] = vet[y]*multiplier;
+		
+		multiplier = multiplier/10;
+		
+	}
+	
+	for(int y = 0; y <= qnt; y++){
+		
+		soma = soma + vet[y];
+		
+	}
+	
+	return soma;	
+	
+}
+
+int comparAlgarismos(int value, int comparador){
+	
+	if(value == comparador){
+		
+		return 1;
+		
+	}
+	else{
+		
+		return 0;
+		
+	}
+}
+
+int main(){
+
+	int value, comparador;
+	
+	printf("Digite um numero: ");
+	scanf("%i", &value);
+	
+	int vet[numAlgarismos(value)];
+	
+	separAlgarismos(value, numAlgarismos(value), vet);
+	
+	comparador = invertAlgarismos(numAlgarismos(value), vet);
+	
+	printf("\n%i", comparAlgarismos(value, comparador));
+	
+}
--- palindromo/palindromo.cpp
+++ palindromo/palindromo.cpp
@@ -1,29 +1,34 @@
-#include <stdio.h>;
-#include <locale.h>;
-#include <string.h>;
+#include <stdio.h>
+#include <locale.h>
+#include <string.h>
 
 void strRemoveSpace(char *str){
-	
-	int len = strlen(str);
-	
-	for(int y = 0; y <= len; y++){
 
-		str2[counter] = str[y];
-		
+	char str2[strlen(str)];
+
+	for(int y = 0, counter = 0; y <= strlen(str); y++){
+
+		if(str[y] == ' '){
+			continue;
+		}
+		else{
+
+			str2[counter] = str[y];
+			counter += 1;
+			printf("\n%c", str2[counter]);
+		}
 	}
 }
 
 void strInverter(char str[], char *str2){
 	
-	int len = strlen(str);
-	
-	for(int y = len-1, counter = 0; y >= -1; y--, counter++){
+	for(int y = strlen(str)-1, counter = 0; y >= -1; y--, counter++){
 
 		str2[counter] = str[y];
 		
 	}
 	
-	str2[len] = '\0';
+	str2[strlen(str)] = '\0';
 	
 }
 
@@ -31,8 +36,11 @@ int main(){
 	
 	setlocale(LC_ALL, "Portuguese_Brazil");
 	
-	char str[] = "Paralelepípedo";
-	
-	char str2[] = "Paralelepípedo";
-	
+	char str[] = "Paralelepípedo casual";
+	char str2[strlen(str)];
+
+	strInverter(str, str2);
+
+	strRemoveSpace(str2);
+
 }
