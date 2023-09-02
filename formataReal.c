788a00bb761655c39442688285ac91f52812fb53
Luiz G. <guga.santos0810@gmail.com>
2023-09-02 00:11:26 -0300

    udpate

--- formataReal.c
+++ formataReal.c
@@ -16,23 +16,49 @@ void somaDigitos(int valor, int *numeroDigitos, int *digitoDivisivel){
 
 }
 
+void somaPontos(int numeroDigitos, int *numeroPontos){
+
+    int y, x;
+
+    for(y = 1, x = 1; y < numeroDigitos; y++, x++){
+
+        if(x == 3){
+
+            x = 1;
+            *numeroPontos += 1;
+
+        }
+    }
+}
+
+void acrescentaPonto(int numeroDigitos){
+
+    for(int y = numeroDigitos-1; y > -1 ; y--){
+
+        //printf("\n%c", str[y]);
+
+    }
+
+}
+
 int main(){
 
     setlocale(LC_ALL, "Portuguese");
 
     double valor;
 
-    int numeroDigitos, digitoDivisivel;
+    int numeroDigitos, digitoDivisivel, numeroPontos = 0;
 
-    printf("Digite um valor: ");
+    printf("Digite um valor:");
     scanf("%lf", &valor);
 
     somaDigitos((int)valor, &numeroDigitos, &digitoDivisivel);
+    somaPontos(numeroDigitos, &numeroPontos);
 
-    char str[numeroDigitos+3];
+    //char str[numeroDigitos+3];
 
-    sprintf(str, "%.2lf", valor);
+    //sprintf(str, "%.2lf", valor);
 
-    
+    printf("\n%i", numeroPontos);
     
 }
