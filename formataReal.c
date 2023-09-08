0ab602343fdf52e3c8db18c816909975d47ce563
Luiz G. <guga.santos0810@gmail.com>
2023-09-08 10:31:03 -0300

    update

--- formataReal.c
+++ formataReal.c
@@ -1,86 +1,174 @@
 #include <stdio.h>
-#include <string.h>
-#include <locale.h>
 #include <stdlib.h>
 
-void somaDigitos(int valor, int *numeroDigitos, int *digitoDivisivel){
+int somaDigitos(int valor){
 
-    int y, contador;
+	int x, y;
 
-    for(y = 1, contador = 1; valor/y > 9; y *= 10, contador++){
+	for(x = 1, y = 1; valor/y > 9; x++, y*=10){
 
-    }
+	}
 
-    *digitoDivisivel = y;
-    *numeroDigitos = contador;
+	return x;
 
 }
 
-void somaPontos(int numeroDigitos, int *numeroPontos){
+int strlen(char *str){
 
-    for(int y = 0, x = 0; y < numeroDigitos; y++, x++){
+	int contador = 0;
 
-        if(x == 3){
+	while(1){
 
-        x = 0;
+		if(str[contador] == '\0'){
 
-        *numeroPontos += 1;
+			break;
 
-        }
-    }
+		}
+		else{
+
+			contador += 1;
+
+		}
+	}
+
+	return contador;
+
 }
 
-void acrescentaPontos(char *str, int numeroPontos) {
+//funcao conta quantos elementos um array de caracteres possui
 
-    int x, y, z, tamanho = strlen(str);
+void substituiPonto(char *str){
 
-    char *v = (char *) malloc(tamanho);
-   
-    for(y = tamanho-1; y > tamanho-4; y--, x++){
-   
-        v[x] = str[y];
-   
-    }
-   
-    for(y = tamanho-4; y > -1; x++, z++){
-   
-        if(z != 3){
-   
-        v[x] = str[y];
-        y -= 1;
-   
-        }
-        else if( z == 3){
-   
-            v[x] = '.';
-            z = -1;
-   
-        }
-    }
+	for(int y = 0; y < strlen(str); y++){
 
-    v[x] = '\0';
+		if(str[y] == '.'){
 
-    printf(v);
+			str[y] = ',';
+
+		}
+	}
+}
+
+void inverteStr(char *str){
+
+	char str2[strlen(str)];
+
+	int x, y;
+
+	for(x = 0, y = strlen(str)-1; y > -1; x++, y--){
+
+		str2[x] = str[y];
+
+	}
+
+	for(y = 0; y < strlen(str); y++){
+
+		str[y] = str2[y];
+
+	}
+}
+
+int somaPonto(int numDigitos){
+
+	int x, y, z;
+
+	for(x = 0, y = 0, z = 0; x < numDigitos; x++, y++){
+
+		if(y == 3){
+
+			y = 0;
+			z += 1;
+
+		}
+	}
+
+	return z;
+
+}
+
+void atribuiPonto(char *str, char *str2, int tamanho){
+
+	int x, y, z;
+
+	inverteStr(str);
+
+	//inverte a str para poder adicionar o ponto ao lugar correto
+
+	for(x = 0; x < 3; x++){
+
+		str2[x] = str[x];
+
+	}
+
+	//atribui as duas casas decimais e a virgula ao segundo array
+
+	for(x = 3, y = 0, z = 3; x < tamanho; x++, y++){
+
+		if(y == 3){
+
+			y = -1;
+			str2[x] = '.';
+
+		}
+		else{
+
+			str2[x] = str[z];
+			z++;
+
+		}
+	}
+
+	str2[x] = '\0';
+
+	//atribui o ponto a segunda string
+
+	inverteStr(str2);
+
+	//inverte a segunda string
+
 }
 
 int main(){
 
-    setlocale(LC_ALL, "Portuguese");
+	double valor;
 
-    double valor;
+	printf("Digite um valor: ");
+	scanf("%lf", &valor);
 
-    int numeroDigitos, digitoDivisivel, numeroPontos = 0;
+	int numDigitos = somaDigitos((int)valor);
 
-    printf("Digite um valor: ");
-    scanf("%lf", &valor);
+	//atribui a variavel numDigitos o numero de digitos
 
-    somaDigitos((int)valor, &numeroDigitos, &digitoDivisivel);
-    somaPontos(numeroDigitos, &numeroPontos);
+	char str[numDigitos+3];
 
-    char str[numeroDigitos+3];
+	//soma 3 para acrescentar o espaco para virgula e duas casas decimais
 
-    sprintf(str, "%.2lf", valor);
+	sprintf(str, "%.2f", valor);
 
-    acrescentaPontos(str, numeroPontos);
-   
+	//atribui ao array de caracteres str o valor digitado com duas casas decimais
+
+	substituiPonto(str);
+
+	//substitui o ponto por virgula
+
+	int numPonto = somaPonto(numDigitos);
+
+	//soma quantos pontos serao necessarios para acrescentar ao numero de elementos de um array
+
+	int tamanho = strlen(str) + numPonto;
+
+	//atribui a variavel tamanho o numero de elementos que sera necessario um array possuir considerando o ponto
+
+	char *str2;
+
+	str2 = (char *)malloc(tamanho);
+
+	//cria um array dinamicamente com o numero de elementos necessarios
+
+	atribuiPonto(str, str2, tamanho);
+
+	//formata o valor da primeira string e atribui para a segunda string
+
+	printf(str2);
+
 }
