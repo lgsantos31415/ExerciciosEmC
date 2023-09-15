e4a3f3e8e04545103e9104f26a006769ce219c22
Luiz G. <guga.santos0810@gmail.com>
2023-09-15 14:54:04 -0300

    update

--- buscaSequencial.c
+++ buscaSequencial.c
@@ -2,54 +2,46 @@
 #include <stdlib.h>
 #include <locale.h>
 
-void declaraArray(int** array, int tamanho){
-	(*array) = (int*)malloc(sizeof(int)*tamanho);
+void declaraVetor(int **vetor, int tamanho){
+    (*vetor) = (int*)malloc(sizeof(int)*tamanho);
 }
 
 int main(){
 	setlocale(LC_ALL, "Portuguese");
 	
-	int *array, tamanho = 0, entrada, contador = 0;
+	int *vetor, tamanho = 0, entrada, contador = 0;
 	
 	while(1){
-		printf("\nEscolha uma das opções:\n1 - Declarar Vetor\n2 - Encontrar elemento\n3 - Sair\n");
+		printf("\nEscolha uma das opções:\n1 - Declarar vetor\n2 - Encontrar elemento\n3 - Sair\n");
 		scanf("%i", &entrada);
 		
 		if(entrada == 1){
-			contador = 0;
 				
 			printf("\nDigite o tamanho do vetor: ");
 			scanf("%i", &tamanho);
 				
-			declaraArray(&array, tamanho);
+			declaraVetor(&vetor, tamanho);
 				
 			printf("\nDigite os elementos do vetor:\n");
 				
-			while(1){
-				if(contador == tamanho){
-					break;
-				}
-				else{
-					scanf("%i", &entrada);
-					array[contador] = entrada;
-					contador += 1;
-				}
-			}
+			for(int y = 0; y < tamanho; y++){
+                scanf("%i", &entrada);
+                vetor[y] = entrada;
+            }
 		}
 		
 		else if(entrada == 2){
-			contador = 0;
-			
 			if(tamanho == 0){
 				printf("\nDeclare um vetor\n\n");
 			}
 			else{
-				
+                contador = 0;
+
 				printf("\nQual elemento você deseja encontrar? ");
 				scanf("%i", &entrada);
 				
 				for(int y = 0; y<tamanho; y++){
-					if(entrada == array[y]){
+					if(entrada == vetor[y]){
 						printf("\nElemento %i na posição %i\n", entrada, y);
 						contador += 1;
 					}
